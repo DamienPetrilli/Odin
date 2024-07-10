77adf0a624820ea1fa368af9a0120d4363af2302
Damien Petrilli <contact@damienpetrilli.com>
2024-07-10 17:22:15 +0200

    tab vs space issue

--- src/build_settings.cpp
+++ src/build_settings.cpp
@@ -162,7 +162,7 @@ struct TargetMetrics {
 enum Subtarget : u32 {
 	Subtarget_Default,
 	Subtarget_iOS,
-    Subtarget_iPhoneSimulator,
+	Subtarget_iPhoneSimulator,
 
 	Subtarget_COUNT,
 };
@@ -170,7 +170,7 @@ enum Subtarget : u32 {
 gb_global String subtarget_strings[Subtarget_COUNT] = {
 	str_lit(""),
 	str_lit("ios"),
-    str_lit("ios-simulator"),
+	str_lit("ios-simulator"),
 };
 
 
@@ -1598,7 +1598,7 @@ gb_internal void init_build_context(TargetMetrics *cross_target, Subtarget subta
 		}
 	}
 
-    if (metrics->os == TargetOs_darwin && subtarget == Subtarget_iPhoneSimulator) {
+	if (metrics->os == TargetOs_darwin && subtarget == Subtarget_iPhoneSimulator) {
 		switch (metrics->arch) {
 		case TargetArch_arm64:
 			bc->metrics.target_triplet = str_lit("arm64-apple-ios-simulator");
